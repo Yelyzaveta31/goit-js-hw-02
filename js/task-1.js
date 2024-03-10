4e8330ee8c505d2ad43956f982d00664d6e079d5
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-03-10 13:10:21 +0200

    task-2 and task-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-makeTransaction(quantity, pricePerDroid, customerCredits)
+function makeTransaction(quantity, pricePerDroid, customerCredits)
 {
     const totalPrice = quantity * pricePerDroid;
     if (totalPrice > customerCredits) {
-       return "Insufficient funds!"
+        return `Insufficient funds!`;
     } 
-    else { return `You ordered ${quantity} droids worth ${totalPrice} credits!` 
+    else { return `You ordered ${quantity} droids worth ${totalPrice} credits!`; 
     }
 };
 
---
+++ js/task-4.js
@@ -0,0 +1,11 @@
+// China — 100 кредитів
+// Chile — 250 кредитів
+// Australia — 170 кредитів
+// Jamaica — 120 кредитів
+
+
+
+
+function getShippingCost(country){
+    
+}
